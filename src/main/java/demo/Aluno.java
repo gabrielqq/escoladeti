4b455daafeebc2d0acd268484ccbd66034a744d4
gabrielqq <gabrielmmaldonado@gmail.com>
2015-02-24 14:14:23 -0300

    EscolaDeTI
    Primeiro Commit sobre a aula do dia 20/02.

---
+++ src/main/java/demo/Aluno.java
@@ -0,0 +1,33 @@
+package demo;
+
+public class Aluno {
+	private String nome;
+	private int ra;
+	
+
+	public Aluno(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getnome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getRa() {
+		return ra;
+	}
+
+	public void setRa(int ra) {
+		this.ra = ra;
+	}
+
+}
